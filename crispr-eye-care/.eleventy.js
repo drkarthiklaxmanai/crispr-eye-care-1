6318cf608a1b323c8035eb2912f7fd47dcae487b
drkarthiklaxmanai <drkarthiklaxmanai@gmail.com>
2026-04-27 10:52:22 +0530

    Update .eleventy.js

--- crispr-eye-care/.eleventy.js
+++ crispr-eye-care/.eleventy.js
@@ -1,1 +1,21 @@
 
+// .eleventy.js
+export default function(eleventyConfig) {
+  // Copy assets folder
+  eleventyConfig.addPassthroughCopy("src/assets");
+
+  // Watch Tailwind CSS changes
+  eleventyConfig.addWatchTarget("./src/assets/css/");
+
+  return {
+    dir: {
+      input: "src",
+      output: "_site",
+      includes: "_includes",
+      layouts: "_includes/layouts"
+    },
+    templateFormats: ["njk", "html", "md"],
+    htmlTemplateEngine: "njk",
+    markdownTemplateEngine: "njk"
+  };
+};
